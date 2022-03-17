0f5370f2ef3402157b846510b5338cc31fb138dd
Hitchhiker1998 <xuchunebi1998@gmail.com>
2022-03-17 21:42:56 +0400

    First Commit

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+module.exports = {
+    googleProjectID: "chat-application-319017",
+    dialogFlowSessionID: "bot-session",
+    dialogFlowSessionLanguageCode: "en-US",
+    googleClientEmail:
+    "chat-bot-app-test@chat-application-319017.iam.gserviceaccount.com",
+    googlePrivateKey:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3Ktdyzwk99IVg\n9TjHuDxZX+Ixo/+pc6BvX24P+oljE8KeeIe3lwCYGOntv+oNQr1qfzLBE1gLq9S9\nYjIwnUWV9o4GZlCAer3XFbzbWVN4SZKg4+jiHsa8P9BRDA5K1ZiizzNjAq1OfUv4\nQdoW3h7zpsj2m9xW9cmCHfq7oEWGwJXga5LcoIMSBN7KUHGKFrsQqphRUPcFo5am\nld1mWbVMtoqIHLfVAOeIoiCiRda2CAatedzQEtHz0FfaQDyuOT3DnRHfDIKnFdLc\nYVClVDZkU61Nd0lueWuJ/orQEr3G7QSqzphMR2dHsMjwVmAxtYSnNKo8rDtVhOw/\n5SY4Y3ATAgMBAAECggEAVIplC5wMZ3ibMS+tx9y8mgG77G6F24e/Nnhk2+ImlEC0\nFKGL/ownHYxuOCpalElIFMXlvqC3hBeIpuqSQMbuLbOxRLnnkQ1V3tatJs65TG0k\nzLa0r91Fm/CWLTozxI2GmCqrTJkZNX92V3zJdMGLpqjF27D/h3GFUtWUexIT1Qf5\nnxNEtmLiKiwmLeFjuLAbKF1XQvfgo3wm/m7veoLMTmXsDsBXx6sLYUCaCoM279Ro\n6iisDc+nrHZqDm4XkOnAJXGUswovNPguB8/F7lPhev1H4eiFELqf636vgJcDSFTx\nuvov0N5ecTfKX7HMATBrHOkRdFRZ4zi7t83M0xjOCQKBgQDaLtlpld/h86Dbrlln\nd+jPN+ou1C6q9zfKsvUPSDL28OfTtuJTAVYQgp7NE9Wq5AdQ32bOKGATaS292hFg\nJoAUMdIHwabKgyeHIB4Rj0FXJ1jED2CC8CNuJCBLurkXAjb0p1xYKQyppJJ1FD+X\nd1HvSak0wTCJHGFIEzVWpXOGqQKBgQDW6knIAMCVF4y64SqHXT7WGtqKjVGXy4EM\nTAB1EoZWWjKRYx9Qw7/e4zZchbBzKBEQNFJ8wwdInBO/DuumlviqbL07BMH0Jt1D\nesafkgccGS/ZhT6QwiMwI2FP74LPJ//3mQNvTGoI7w2F5yYkps0vYXYyOdb8uXHr\nEQUzEqNCWwKBgQCFErYANC1M/u9Jjdlxxiz01kG7NicQl6ArvWLCY2LSQrTi6FS6\nE8KOZ1y78DEN/e6IYPmvG+kdbbU8aljWsCTGLGzR1kcIlV3yiKBOTXWo1pLJYieX\nNSgm8VnRVp21buBd8AXer4rNnVbGmgb/NwN+Uwsk5f/YYLrk3vJVWqxPoQKBgDkq\nKGXijVca5Hvw2HcY/lXE3sxruoA9yR2UOarqOoFNjJXPX80DxE1s8lC53q1Cwwck\nY6x707wkh7wSI+wzzADI26yPH2fpzoDcb2iSVwHKUUfygSnP+bwcLQ4uDwU7BoK9\nWISKNsoZi+dKDINDOuCXc/D4IY8nZH26ET/8fSBxAoGAWXoceLND8Tfjx2d+WmUm\n4pQaPGx85/Ez0C51iDRkiE729xtXlQ2/dr9UgIUL76/alFKq1JCi2tFI+YZ6xvly\ncnHjIVvMLuFl14uyLOBEErtibWwB0t8SDgMlx7DnJqDxO1U9u4VW6A9X32eeq3x1\n8JWHxaSWc/JYXyc+mrYuJVw=\n-----END PRIVATE KEY-----\n",
+    mongoURI: "",
+    encoding: "AUDIO_ENCODING_LINEAR_16"
+};
+
+
+//set GOOGLE_APPLICATION_CREDENTIALS=/D:\Desktop\Portfolio\DialogFlow Key/chat-application-319017-2646ce6f61d1.json
---
+++ server.js
@@ -0,0 +1,167 @@
+const config = require("./config/keys");
+const projectId = config.googleProjectID;
+const port = config.npm_config_PORT || 3000;
+
+const languageCode = config.dialogFlowSessionLanguageCode;
+let encoding = "AUDIO_ENCODING_LINEAR_16";
+
+const singleUtterance = false;
+const interimResults = false;
+const sampleRateHertz = 16000;
+const speechContexts = [
+  {
+    phrases: ["mail", "email"],
+    boost: 20.0,
+  },
+];
+
+console.log(projectId);
+
+// ----------------------
+
+// load all the libraries for the server
+const socketIo = require("socket.io");
+const path = require("path");
+const fs = require("fs");
+const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const ss = require("socket.io-stream");
+// load all the libraries for the Dialogflow part
+const uuid = require("uuid");
+const util = require("util");
+const { Transform, pipeline } = require("stream");
+const pump = util.promisify(pipeline);
+const dialogflow = require("dialogflow").v2beta1;
+
+// set some server variables
+const app = express();
+var server;
+var sessionId, sessionClient, sessionPath, request;
+var speechClient,
+  requestSTT,
+  ttsClient,
+  requestTTS,
+  mediaTranslationClient,
+  requestMedia;
+
+/**
+ * Setup Express Server with CORS and SocketIO
+ */
+function setupServer() {
+  // setup Express
+  app.use(cors());
+  app.use(express.static("public"));
+  // app.get('/', function(req, res) {
+  //   res.sendFile(path.join(__dirname + '/Test_copy'+'.html'));
+  // });
+  server = http.createServer(app);
+  io = socketIo(server);
+  server.listen(port, () => {
+    console.log("Running server on port %s", port);
+  });
+
+  // Listener, once the client connect to the server socket
+  io.on("connect", (client) => {
+    console.log(`Client connected [id=${client.id}]`);
+    client.emit("server_setup", `Server connected [id=${client.id}]`);
+
+    // when the client sends 'message' events
+    // when using simple audio input
+    client.on("message", async function (data) {
+      // we get the dataURL which was sent from the client
+      console.log("data - 72", data.audio.dataURL);
+      const dataURL = data.audio.dataURL.split(",").pop();
+      // we will convert it to a Buffer
+      let fileBuffer = Buffer.from(dataURL, "base64");
+      // run the simple detectIntent() function
+      const results = await detectIntent(fileBuffer);
+      client.emit("results", results);
+    });
+
+    // when the client sends 'message-transcribe' events
+    // when using simple audio input
+    client.on("message-transcribe", async function (data) {
+      // we get the dataURL which was sent from the client
+      const dataURL = data.audio.dataURL.split(",").pop();
+      // we will convert it to a Buffer
+      let fileBuffer = Buffer.from(dataURL, "base64");
+      // run the simple transcribeAudio() function
+      const results = await transcribeAudio(fileBuffer);
+      client.emit("results", results);
+    });
+    // when the client sends 'text-message' events
+    // when using text input
+    client.on("textMessage",  async function (data) {
+      console.log('Iaminbackend')
+      console.log(data)
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            // The query to send to the dialogflow agent
+            text: data,
+            // The language used by the client (en-US)
+            languageCode: languageCode,
+          },
+        },
+      };
+      // Send request and log result
+      const results = await sessionClient.detectIntent(request);
+      results[0].queryResult.text = true;
+      client.emit("results", results);
+      console.log(results)
+    });
+    
+  });
+}
+
+/**
+ * Setup Dialogflow Integration
+ */
+function setupDialogflow() {
+  // Dialogflow will need a session Id
+  sessionId = config.dialogFlowSessionID;
+  // Dialogflow will need a DF Session Client
+  // So each DF session is unique
+  sessionClient = new dialogflow.SessionsClient();
+  // Create a session path from the Session client,
+  // which is a combination of the projectId and sessionId.
+  sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+  // Create the initial request object
+  // When streaming, this is the first call you will
+  // make, a request without the audio stream
+  // which prepares Dialogflow in receiving audio
+  // with a certain sampleRateHerz, encoding and languageCode
+  // this needs to be in line with the audio settings
+  // that are set in the client
+  request = {
+    session: sessionPath,
+    queryInput: {
+      audioConfig: {
+        sampleRateHertz: sampleRateHertz,
+        encoding: encoding,
+        languageCode: languageCode,
+        speechContexts: speechContexts,
+      },
+      singleUtterance: singleUtterance,
+    },
+  };
+}
+
+/*
+ * Dialogflow Detect Intent based on Audio
+ * @param audio file buffer
+ * @param cb Callback function to execute with results
+ */
+async function detectIntent(audio) {
+  request.inputAudio = audio;
+  console.log("request-150", request);
+  const responses = await sessionClient.detectIntent(request);
+  console.log("response - 143", responses);
+  return responses;
+}
+
+setupDialogflow();
+setupServer();
